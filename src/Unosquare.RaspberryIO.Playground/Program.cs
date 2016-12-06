8e784e434884f4ea6645ac36d4f219bc56dc55f3
Mario Di Vece <mario@unosquare.com>
2016-12-06 17:53:21 -0600

    adding test blinking LED example

--- src/Unosquare.RaspberryIO.Playground/Program.cs
+++ src/Unosquare.RaspberryIO.Playground/Program.cs
@@ -24,6 +24,27 @@
             }
         }
 
+        public static void TestLedBlinking()
+        {
+            // Get a reference to the pin you need to use.
+            // All 3 methods below are exactly equivalente
+            var blinkingPin = Pi.Gpio[0];
+            blinkingPin = Pi.Gpio[WiringPiPin.Pin00];
+            blinkingPin = Pi.Gpio.Pin00;
+
+            // Configure the pin as an output
+            blinkingPin.PinMode = GpioPinDriveMode.Output;
+
+            // perform writes to the pin by toggling the isOn variable
+            var isOn = false;
+            for (var i = 0; i < 20; i++)
+            {
+                isOn = !isOn;
+                blinkingPin.Write(isOn);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
         private static void TestSystemInfo()
         {
             Console.WriteLine($"GPIO Controller initialized successfully with {Pi.Gpio.Count} pins");
