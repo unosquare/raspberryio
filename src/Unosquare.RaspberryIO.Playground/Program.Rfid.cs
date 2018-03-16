ddf5b4605985a981c83f0800bed22d34920fa338
Mario Di Vece <mario@unosquare.com>
2018-03-16 12:51:28 -0600

    fixing and cleaning up code for RFID Controller. This has not yet been tested.

---
+++ src/Unosquare.RaspberryIO.Playground/Program.Rfid.cs
@@ -0,0 +1,46 @@
+﻿namespace Unosquare.RaspberryIO.Playground
+{
+    using Peripherals;
+    using Swan;
+
+    public partial class Program
+    {
+        private static void TestRfidController()
+        {
+            var device = new RFIDControllerMfrc522();
+
+            while (true)
+            {
+                // If a card is found
+                if (device.DetectCard() == RFIDControllerMfrc522.Status.AllOk)
+                    "Card detected".Info();
+
+                // Get the UID of the card
+                var uidResponse = device.ReadCardUniqueId();
+
+                // If we have the UID, continue
+                if (uidResponse.Status == RFIDControllerMfrc522.Status.AllOk)
+                {
+                    var cardUid = uidResponse.Data;
+
+                    // Print UID
+                    $"Card UID: {cardUid[0]},{cardUid[1]},{cardUid[2]},{cardUid[3]}".Info();
+
+                    // Select the scanned tag
+                    device.SelectCardUniqueId(cardUid);
+
+                    // Check if authenticated
+                    if (device.AuthenticateCard1A(RFIDControllerMfrc522.DefaultAuthKey, cardUid) == RFIDControllerMfrc522.Status.AllOk)
+                    {
+                        device.ReadRegister(RFIDControllerMfrc522.Register.Status2Reg);
+                        device.ClearCardSelection();
+                    }
+                    else
+                    {
+                        "Authentication error".Error();
+                    }
+                }
+            }
+        }
+    }
+}
