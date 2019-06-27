9f7efc046e1ca4e542344ac02bf5f25b51e09920
Ramiro Flores <Serk352@gmail.com>
2019-06-27 11:46:21 -0500

    working on write card

--- src/Unosquare.RaspberryIO.Playground/Program.Rfid.cs
+++ src/Unosquare.RaspberryIO.Playground/Program.Rfid.cs
@@ -162,9 +162,22 @@
 
             while (true)
             {
+                // If a card is found
+                if (device.DetectCard() != RFIDControllerMfrc522.Status.AllOk) continue;
+
+                // Get the UID of the card
                 var uidResponse = device.ReadCardUniqueId();
+
+                // If we have the UID, continue
+                if (uidResponse.Status != RFIDControllerMfrc522.Status.AllOk) continue;
+
                 var cardUid = uidResponse.Data;
 
+                // Select the scanned tag
+                device.SelectCardUniqueId(cardUid);
+
+                // Writing data to sector 1 blocks
+                // Authenticate sector
                 if (device.AuthenticateCard1A(RFIDControllerMfrc522.DefaultAuthKey, cardUid, 7) == RFIDControllerMfrc522.Status.AllOk)
                 {
                     var data = new byte[16 * 3];
@@ -178,18 +191,55 @@
                         device.CardWriteData((byte)(4 + b), data.Skip(b * 16).Take(16).ToArray());
                     }
                 }
+
+                device.ClearCardSelection();
+                "Writed".Info();
+                break;
             }
         }
 
         private static void ReadCard()
         {
+            "Testing RFID".Info();
             var device = new RFIDControllerMfrc522(Pi.Spi.Channel0, 500000, Pi.Gpio[18]);
 
             while (true)
             {
+                // If a card is found
+                if (device.DetectCard() != RFIDControllerMfrc522.Status.AllOk) continue;
+                "Card detected".Info();
+
+                // Get the UID of the card
                 var uidResponse = device.ReadCardUniqueId();
+
+                // If we have the UID, continue
+                if (uidResponse.Status != RFIDControllerMfrc522.Status.AllOk) continue;
+
                 var cardUid = uidResponse.Data;
 
+                // Print UID
+                $"Card UID: {cardUid[0]},{cardUid[1]},{cardUid[2]},{cardUid[3]}".Info();
+
+                // Select the scanned tag
+                device.SelectCardUniqueId(cardUid);
+
+                // Writing data to sector 1 blocks
+                // Authenticate sector
+                if (device.AuthenticateCard1A(RFIDControllerMfrc522.DefaultAuthKey, cardUid, 7) == RFIDControllerMfrc522.Status.AllOk)
+                {
+                    var data = new byte[16 * 3];
+                    for (var x = 0; x < data.Length; x++)
+                    {
+                        data[x] = (byte)(x + 65);
+                    }
+
+                    for (var b = 0; b < 3; b++)
+                    {
+                        device.CardWriteData((byte)(4 + b), data.Skip(b * 16).Take(16).ToArray());
+                    }
+                }
+
+                // Reading data
                 var continueReading = true;
                 for (var s = 0; s < 16 && continueReading; s++)
                 {
@@ -215,6 +265,8 @@
                         break;
                     }
                 }
+
+                device.ClearCardSelection();
             }
         }
     }
