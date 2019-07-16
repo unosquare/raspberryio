d69731dfe6dfc509b96e0293c9f4d20f81062f59
carlos.solorzano <carlos.solorzano@unosquare.com>
2019-07-16 16:47:26 -0500

    min fix

--- src/Unosquare.RaspberryIO.Playground/Extra/Extra.Led.cs
+++ src/Unosquare.RaspberryIO.Playground/Extra/Extra.Led.cs
@@ -13,29 +13,7 @@
         {
             using (var cancellationTokenSource = new CancellationTokenSource())
             {
-                Task.Run(() =>
-                {
-                    Console.Clear();
-                    var blinkingPin = Pi.Gpio[BcmPin.Gpio13];
-
-                    // Configure the pin as an output
-                    blinkingPin.PinMode = GpioPinDriveMode.Output;
-
-                    // perform writes to the pin by toggling the isOn variable
-                    var isOn = false;
-                    while (!cancellationTokenSource.Token.IsCancellationRequested)
-                    {
-                        isOn = !isOn;
-                        blinkingPin.Write(isOn);
-                        var ledState = isOn ? "on" : "off";
-                        Console.Clear();
-                        $"Blinking {ledState}".Info();
-                        "Press Esc key to continue . . .".WriteLine();
-                        Thread.Sleep(500);
-                    }
-
-                    blinkingPin.Write(0);
-                });
+                Blink(cancellationTokenSource.Token);
 
                 while (true)
                 {
@@ -52,33 +30,7 @@
         {
             using (var cancellationTokenSource = new CancellationTokenSource())
             {
-                Task.Run(() =>
-                {
-                    Console.Clear();
-                    "Dimming".Info();
-                    "Press Esc key to continue . . .".WriteLine();
-                    var pin = (GpioPin)Pi.Gpio[BcmPin.Gpio13];
-                    pin.PinMode = GpioPinDriveMode.PwmOutput;
-                    pin.PwmMode = PwmMode.Balanced;
-                    pin.PwmClockDivisor = 2;
-                    while (!cancellationTokenSource.Token.IsCancellationRequested)
-                    {
-                        for (var x = 0; x <= 100; x++)
-                        {
-                            pin.PwmRegister = (int)pin.PwmRange / 100 * x;
-                            Thread.Sleep(10);
-                        }
-
-                        for (var x = 0; x <= 100; x++)
-                        {
-                            pin.PwmRegister = (int)pin.PwmRange - ((int)pin.PwmRange / 100 * x);
-                            Thread.Sleep(10);
-                        }
-                    }
-
-                    pin.PinMode = GpioPinDriveMode.Output;
-                    pin.Write(0);
-                });
+                Dim(cancellationTokenSource.Token);
 
                 while (true)
                 {
@@ -90,5 +42,59 @@
                 }
             }
         }
+
+        private static void Blink(CancellationToken cancellationToken) =>
+            Task.Run(() =>
+            {
+                Console.Clear();
+                var blinkingPin = Pi.Gpio[BcmPin.Gpio13];
+
+                        // Configure the pin as an output
+                        blinkingPin.PinMode = GpioPinDriveMode.Output;
+
+                        // perform writes to the pin by toggling the isOn variable
+                        var isOn = false;
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    isOn = !isOn;
+                    blinkingPin.Write(isOn);
+                    var ledState = isOn ? "on" : "off";
+                    Console.Clear();
+                    $"Blinking {ledState}".Info();
+                    "Press Esc key to continue . . .".WriteLine();
+                    Thread.Sleep(500);
+                }
+
+                blinkingPin.Write(0);
+            });
+
+        private static void Dim(CancellationToken cancellationToken) =>
+            Task.Run(() =>
+            {
+                Console.Clear();
+                "Dimming".Info();
+                "Press Esc key to continue . . .".WriteLine();
+                var pin = (GpioPin)Pi.Gpio[BcmPin.Gpio13];
+                pin.PinMode = GpioPinDriveMode.PwmOutput;
+                pin.PwmMode = PwmMode.Balanced;
+                pin.PwmClockDivisor = 2;
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    for (var x = 0; x <= 100; x++)
+                    {
+                        pin.PwmRegister = (int)pin.PwmRange / 100 * x;
+                        Thread.Sleep(10);
+                    }
+
+                    for (var x = 0; x <= 100; x++)
+                    {
+                        pin.PwmRegister = (int)pin.PwmRange - ((int)pin.PwmRange / 100 * x);
+                        Thread.Sleep(10);
+                    }
+                }
+
+                pin.PinMode = GpioPinDriveMode.Output;
+                pin.Write(0);
+            });
     }
 }
