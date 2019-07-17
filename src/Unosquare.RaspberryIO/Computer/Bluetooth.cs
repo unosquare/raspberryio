e75834a79213d7e9c2fe4479dfbf0be6bfeb2e37
Ramiro Flores <Serk352@gmail.com>
2019-07-17 16:14:46 -0500

    adding requested class

---
+++ src/Unosquare.RaspberryIO/Computer/Bluetooth.cs
@@ -0,0 +1,26 @@
+﻿namespace Unosquare.RaspberryIO.Computer
+{
+    using System.Collections.Generic;
+    using Unosquare.Swan.Abstractions;
+
+    /// <summary>
+    /// Represents the bluetooth information.
+    /// </summary>
+    public class Bluetooth : SingletonBase<Bluetooth>
+    {
+        public bool PowerOn()
+        {
+            return true;
+        }
+
+        public bool PowerOff()
+        {
+            return true;
+        }
+
+        public List<string> ListDevices()
+        {
+            return new List<string>();
+        }
+    }
+}
