9131a4ebf2860916b6d319283db6a1d7b4cbd809
Ramiro Flores <Serk352@gmail.com>
2019-07-19 16:20:39 -0500

    Update Bluetooth.cs

--- src/Unosquare.RaspberryIO/Computer/Bluetooth.cs
+++ src/Unosquare.RaspberryIO/Computer/Bluetooth.cs
@@ -1,6 +1,10 @@
-﻿namespace Unosquare.RaspberryIO.Computer
+﻿using Unosquare.Swan;
+using Unosquare.Swan.Components;
+
+namespace Unosquare.RaspberryIO.Computer
 {
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Unosquare.Swan.Abstractions;
 
     /// <summary>
@@ -8,19 +12,35 @@
     /// </summary>
     public class Bluetooth : SingletonBase<Bluetooth>
     {
-        public bool PowerOn()
+        public async Task<bool> PowerOn()
         {
+            var openBluetoothTool = await ProcessRunner.GetProcessOutputAsync("bluetoothctl").ConfigureAwait(false); //Open bluetooth tool
+            $"{openBluetoothTool}".Info();
+
+            var turnOnBluetoothTool = await ProcessRunner.GetProcessOutputAsync("power on").ConfigureAwait(false); //turn on bluetooth
+            $"{turnOnBluetoothTool}".Info();
+
             return true;
         }
 
-        public bool PowerOff()
+        public async Task<bool> PowerOff()
         {
+
+            var openBluetoothTool = await ProcessRunner.GetProcessOutputAsync("bluetoothctl").ConfigureAwait(false); //Open bluetooth tool
+            $"{openBluetoothTool}".Info();
+
+            var turnOnBluetoothTool = await ProcessRunner.GetProcessOutputAsync("power off").ConfigureAwait(false); //turn on bluetooth
+            $"{turnOnBluetoothTool}".Info();
+
             return true;
         }
 
-        public List<string> ListDevices()
+        public async Task<List<string>> ListDevices()
         {
+            var openBluetoothTool = await ProcessRunner.GetProcessOutputAsync("list").ConfigureAwait(false); //Open bluetooth tool
+            $"{openBluetoothTool}".Info();
             return new List<string>();
+
         }
     }
 }
